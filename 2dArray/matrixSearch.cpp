2a48752d9331b834182be2ebe4332305e559d391
the-tushar-sonar <mylordtushar@gmail.com>
2025-05-10 13:22:47 +0530

    Add implementation of Searching in 2D Array using Brute Force Methon, Binary Search Method & Staircase Search Method

--- 2dArray/matrixSearch.cpp
+++ 2dArray/matrixSearch.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 // Brute Force Method --> O(n*m)
-int search(int mat[][4], int n, int m, int key)
+bool search(int mat[][4], int n, int m, int key)
 {
     for (int i = 0; i < n; i++)
     {
@@ -11,34 +11,85 @@ int search(int mat[][4], int n, int m, int key)
             if (mat[i][j] == key)
             {
                 cout << "key is found at index " << i << "," << j << endl;
+                return true;
             }
         }
     }
-
-    return 0;
+    cout << "Key is not found " << endl;
+    return false;
 }
-// Binary Search Method --> O(n*log m) or O(log n*m)
-int bSearch(int mat[][4], int n, int m, int key)
+
+// Binary Search Method --> O(log (n*m))
+// This is not working for the given matrix because the matrix is not sorted in a single row or column.
+bool bSearch(int mat[][4], int n, int m, int key)
 {
+    int st = 0, end = n * m - 1;
 
-    return 0;
+    while (st <= end)
+    {
+        int mid = st + (end - st) / 2;
+        int row = mid / m; // row index
+        int col = mid % m; // col index
+        int midVal = mat[row][col];
+
+        if (midVal == key)
+        {
+            cout << "key is found at index " << row << "," << col << endl;
+            return true;
+        }
+        else if (midVal < key)
+        {
+            st = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+    cout << "Key is not found " << endl;
+    return false;
 }
+
 // Staircase Search Method --> O(n+m)
-int sSearch(int mat[][4], int n, int m, int key)
+bool sSearch(int mat[][4], int n, int m, int key)
 {
+    int i = 0, j = m - 1;
 
-    return 0;
+    while (i < n && j >= 0)
+    {
+        if (mat[i][j] == key)
+        {
+            cout << "key is found at index " << i << "," << j << endl;
+            return true;
+        }
+        else if (mat[i][j] > key)
+        {
+            // left
+            j--;
+        }
+        else
+        {
+            // down
+            i++;
+        }
+    }
+
+    cout << "Key is not found " << endl;
+    return false;
 }
 
 int main()
 {
+    // 2D array
     int mat[4][4] = {
         {10, 20, 30, 40},
         {15, 25, 35, 45},
         {27, 29, 37, 48},
         {32, 33, 39, 50}};
 
-    search(mat, 4, 4, 29);
+    // search(mat, 4, 4, 29);
+    // bSearch(mat, 4, 4, 29);
+    sSearch(mat, 4, 4, 29);
 
     return 0;
 }
