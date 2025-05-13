c00c0ed071f5967107ebf7d2a26c658f50112431
the-tushar-sonar <mylordtushar@gmail.com>
2025-05-13 18:09:18 +0530

    Add implementation of palindrome checking function for character arrays

---
+++ Char Array & String/validPalindrome.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+//  Leetcode 125
+
+bool isPalindrome(char word[], int n) // O(n)
+{
+    int st = 0, end = n - 1;
+    while (st < end)
+    {
+        if (word[st++] != word[end--])
+        {
+            cout << " Not a Valid Palindrome";
+            return false;
+        }
+    }
+
+    cout << "Valid Palindrome";
+    return true;
+}
+
+int main()
+{
+    char word[] = "madam";
+
+    isPalindrome(word, strlen(word));
+
+    return 0;
+}
