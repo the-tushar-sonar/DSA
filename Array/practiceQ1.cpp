5c950bd7c74c390758b323f03f843078bbbe5044
the-tushar-sonar <mylordtushar@gmail.com>
2025-06-07 20:42:57 +0530

    Add spacing for improved readability & leetcode link in practiceQ1.cpp comments

--- Array/practiceQ1.cpp
+++ Array/practiceQ1.cpp
@@ -4,11 +4,14 @@ using namespace std;
 /*
 Question 1 : Given an integer array nums, return true if any value appears at least
 twice in the array, and return false if every element is distinct. [link]
+
 Examples :
 Input: nums = [1,2,3,4]
 Output: false
 Input: nums = [1,1,1,3,3,4,3,2,4,2]
 Output: true
+
+link : https://leetcode.com/problems/contains-duplicate
 */
 // Brute Force Method
 bool isDuplicate(int *arr, int n) // Time Complexity: O(n^2)
